22b3cbd2138300226f9c14d69980cff3a4cb9afd
marzban-dev <marzban.dev@gmail.com>
2025-08-22 12:38:29 +0330

    Updated

--- composables/useBaseUrl.ts
+++ composables/useBaseUrl.ts
@@ -1,5 +1,11 @@
-import { useRuntimeConfig } from '#imports'
+import { useRuntimeConfig } from "#imports";
 
+/**
+ * Adds two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
 const useBaseUrl = () => {
     const config = useRuntimeConfig();
     return config.public.API_BASE_URL;
--- plugins/event-bus.ts
+++ plugins/event-bus.ts
@@ -1,5 +1,8 @@
 import mitt from "mitt";
 
+/**
+    @
+*/
 export default defineNuxtPlugin(() => {
     const emitter = mitt();
 
